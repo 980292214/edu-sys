4ae3bcc50b51946ad319af8ecf56eb6a190545f9
saltfish2333 <2864799774@qq.com>
2022-01-04 23:11:53 +0800

    x-admin/

--- x-admin/src/main/java/com/example/mapper/selectMapper.java
+++ x-admin/src/main/java/com/example/mapper/selectMapper.java
@@ -2,7 +2,15 @@ package com.example.mapper;
 
 import com.example.entity.select;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface selectMapper extends BaseMapper<select> {
-
+//    @Select(value="SELECT * FROM t_select AS s " +
+//            "INNER JOIN t_course AS c ON s.`courseid` = c.id " +
+//            "INNER JOIN t_teachers AS t ON c.`teacherid`=t.`teacherid` " +
+//            "WHERE s.`studentid`=#{studentId,jdbcType=VARCHAR}")
+    public List findByStudentId(@Param("studentId") String studentId);
 }
--- x-admin/src/main/java/com/example/service/selectService.java
+++ x-admin/src/main/java/com/example/service/selectService.java
@@ -1,11 +1,14 @@
 package com.example.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.entity.select;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.mapper.selectMapper;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class selectService extends ServiceImpl<selectMapper, select> {
@@ -13,4 +16,10 @@ public class selectService extends ServiceImpl<selectMapper, select> {
     @Resource
     private selectMapper selectMapper;
 
+
+    public List findByStudentId(String studentId) {
+
+        List courses = selectMapper.findByStudentId(studentId);
+        return courses;
+    }
 }
